98355ea0c9b40db2ed46ff39ae1592406e65241f
TOG3-LUA <bagaev.ilya.ivanovich@gmail.com>
2024-08-01 22:49:27 +0800

    Update buggati.lua

--- buggati.lua
+++ buggati.lua
@@ -1,4 +1,3 @@
-
 --instances
 
 local gui = Instance.new("ScreenGui")
@@ -10,10 +9,12 @@ local dec1 = Instance.new("UICorner")
 local dec2 = Instance.new("UIStroke")
 local tab = Instance.new("Frame")
 local tab1 = Instance.new("Frame")
+
+--buttons
+
+local toggle = Instance.new("TextButton")
 local button = Instance.new("TextButton")
 local button1 = Instance.new("TextButton")
-local test = Instance.new("TextLabel")
-local close = Instance.new("TextButton")
 
 --properties???
 
@@ -24,18 +25,21 @@ main.Size = UDim2.new(0, 549,0, 355)
 main.Position = UDim2.new(0.266, 0,0.268, 0)
 main.BackgroundColor3 = Color3.fromRGB(35, 35, 35)
 
+toggle.Parent = gui
+
 bar.Parent = main
 bar.Size = UDim2.new(0, 549,0, 22)
 bar.Position = UDim2.new(0, 0,0.286, 0)
 bar.BackgroundColor3 = Color3.fromRGB(63, 63, 63)
 bar.BorderColor3 = Color3.fromRGB(63,63,63)
-
+bar.BorderSizePixel = 0
 
 bar1.Parent = main
 bar1.Size = UDim2.new(0, 20,0, 231)
 bar1.Position = UDim2.new(0.219, 0,0.348, 0)
 bar1.BackgroundColor3 = Color3.fromRGB(63, 63, 63)
 bar1.BorderColor3 = Color3.fromRGB(63,63,63)
+bar1.BorderSizePixel = 0
 
 button.TextColor3 = Color3.fromRGB(255,255,255)
 button.TextScaled = true
@@ -44,7 +48,7 @@ dec1:Clone().Parent = button
 button.Position = UDim2.new(0.011, 0,0.389, 0)
 button.Size = UDim2.new(0, 106,0, 29)
 button.BackgroundColor3 = Color3.fromRGB(71, 71, 71)
-button.Text = "go fuck yourself"
+button.Text = "Players"
 button.Font = Enum.Font.Arimo
 
 button1.TextColor3 = Color3.fromRGB(255,255,255)
@@ -52,36 +56,31 @@ button1.TextScaled = true
 button1.Parent = main
 dec1:Clone().Parent = button1
 button1.Position = UDim2.new(0.011, 0,0.485, 0)
-button1.Size = UDim2.new(0, 106,0, 29)
+button1.Size = UDim2.new(0, 106,0, 29) 
 button1.BackgroundColor3 = Color3.fromRGB(71, 71, 71)
-button1.Text = "go fuck yourself1"
+button1.Text = "Teleports"
 button1.Font = Enum.Font.Arimo
 
-test.TextScaled = true
-test.Parent = tab
-test.Text = "skibidi"
-test.Position = UDim2.new(0.232, 0,0.117, 0)
-test.Size = UDim2.new(0, 200,0, 50)
-
-close.Parent = main
-close.Size = UDim2.new(0, 51,0, 50)
-close.Position = UDim2.new(0.905, 0,0, 0)
-dec1:Clone().Parent = close
-close.BackgroundTransparency = 0.6
-close.TextScaled = true
-close.TextColor3 = Color3.fromRGB(255,255,255)
-close.Text = "Close"
+toggle.Parent = gui
+toggle.Size = UDim2.new(0, 106,0, 29)
+toggle.Position = UDim2.new(0.471, 0,0.205, 0)
+dec1:Clone().Parent = toggle
+toggle.TextScaled = true
+toggle.TextColor3 = Color3.fromRGB(255,255,255)
+toggle.Text = "Toggle"
 
 tab.Parent = main
 tab.Size = UDim2.new(0, 409,0, 231)
 tab.Position = UDim2.new(0.255, 0,0.348, 0)
 tab.BackgroundColor3 = Color3.fromRGB(35, 35, 35)
+tab.BorderSizePixel = 0
 
 tab1.Visible = false
 tab1.Parent = main
 tab1.Size = UDim2.new(0, 409,0, 231)
 tab1.Position = UDim2.new(0.255, 0,0.348, 0)
 tab1.BackgroundColor3 = Color3.fromRGB(35, 35, 35)
+tab1.BorderSizePixel = 0
 
 
 title.Parent = main
@@ -101,10 +100,16 @@ main.Archivable = true
 main.Draggable = true
 main.Active = true
 
+toggle.Archivable = true
+toggle.Draggable = true
+toggle.Active = true
+
 title.TextColor3 = Color3.fromRGB(255, 255, 255)
 title.Font = Enum.Font.Arimo
 title.BackgroundTransparency = 1
-title.Text = "welcome, "..game.Players.LocalPlayer.Name
+title.Text = "Welcome to the worst script ever. Thanks for using it, "..game.Players.LocalPlayer.DisplayName.."."
+
+--FUNCTIONS!!!!!!!!!!!!!!!!!!!!!!!!!!
 
 button.MouseButton1Up:Connect(function()
 	tab.Visible = false
@@ -126,10 +131,11 @@ button1.TouchTap:Connect(function()
 	tab1.Visible = false
 end)
 
-close.MouseButton1Up:Connect(function()
-	gui:Remove()
+toggle.MouseButton1Up:Connect(function()
+	main.Visible = not main.Visible
 end)
 
-close.TouchTap:Connect(function()
-	gui:Remove()
+toggle.TouchTap:Connect(function()
+	main.Visible = not main.Visible
 end)
+
